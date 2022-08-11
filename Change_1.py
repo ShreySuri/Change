a0ea858dfab13e8a260e66ed081b8766be9f71e2
ShreySuri <shreysuri5@gmail.com>
2022-08-10 21:31:34 -0500

    Update Change_1.py

--- Change_1.py
+++ Change_1.py
@@ -3,21 +3,41 @@ def cap(small_num, big_num):
     quotient = int((big_num - rem) / small_num)
     return(quotient)
 
-def higher_than(x, y_list):
+def sift_lower(x, y_list):
     length = len(y_list)
-    index_list = []
+    counter = 0
     for i in range (0, length):
         if y_list[i] > x:
-            index_list.append(i)
+            y_list[i] = "placeholder"
+            counter = counter + 1
         else:
             toggle = True
 
-    return(index_list)
+    if counter > 0:
+        for i in range (0, counter):
+            y_list.remove("placeholder")
+    else:
+        toggle = False
+    
+    return(y_list)
+
 
 def change(amount, currency_list):
+    currency_list = sift_lower(amount, currency_list)
+    length = len(currency_list)
+    max_list = []
+    for i in range(0, length):
+        x = currency_list[i]
+        y = cap(x, amount)
+        max_list.append(y)
 
+    for j in range 
+        
+            
+            
 
-print(change(5, [1, 2, 5, 10, 20, 50, 100]))
+list_1 = [1, 2, 5, 10, 20, 50, 100]
+print(change(5, list_1))
     
             
     
