0ce6171a552a7a6daffb3cf1b95896cc32c248b5
ShreySuri <shreysuri5@gmail.com>
2022-08-11 18:12:34 -0500

    Update Change_1.py

--- Change_1.py
+++ Change_1.py
@@ -55,14 +55,59 @@ def change(amount, currency_list):
 
     return(counter)
 
+def validate_float(x):
+    list_x = list(x)
+    length = len(list_x)
+    accepted_val = []
+    for i in range (0, 10):
+        i = str(i)
+        accepted_val.append(i)
+    accepted_val.append(".")
+    period_count = 0
+
+    counter = 0
+    for j in range (0, length):
+        for k in range (0, 11):
+            if list_x[j] == accepted_val[k]:
+                counter = counter + 1
+                if k == 10:
+                    period_count = period_count + 1
+                else:
+                    toggle = True
+            else:
+                toggle = False
+    if counter == length and period_count < 2:
+        return(True)
+    else:
+        return(False)
+
+amount = "None"
+while validate_float(amount) == False:
+    print("")
+    amount = input("What amount do you want change for? ")
+
+amount = float(amount)
+
+denominations = []
+input_1 = "None"
+while input_1 != "done":
+    print("")
+    input_1 = input("Enter a denomination. If you have entered all denominations, type 'done'. ")
+    input_1 = input_1.lower()
+
+    if validate_float(input_1) == True:
+        input_1 = float(input_1)
+        denominations.append(input_1)
+    elif input_1 != "done":
+        print("Please enter a float value or 'done'.")
+    else:
+        toggle = True
     
     
         
             
             
-amount = 1
-list_1 = [0.01, 0.05, 0.1, 0.25, 0.5, 1, 2, 5, 10, 20, 50, 100]
-print(change(amount, list_1))
+print(change(amount, denominations))
     
             
     
